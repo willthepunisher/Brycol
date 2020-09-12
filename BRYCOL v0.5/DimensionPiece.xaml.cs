7756bee15f58274a8a809aad5bd111096b766eab
William Leclerc <70967043+willthepunisher@users.noreply.github.com>
2020-09-12 19:35:18 -0400

    Add files via upload

---
+++ BRYCOL v0.5/DimensionPiece.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BRYCOL_v0._5
+{
+    /// <summary>
+    /// Logique d'interaction pour DimensionPiece.xaml
+    /// </summary>
+    public partial class DimensionPiece : Window
+    {
+        public DimensionPiece()
+        {
+            InitializeComponent();
+        }
+
+        private void clkContinuer(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BRYCOL v0.5/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BRYCOL_v0._5
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int choixMenu;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        #region canvasTest
+        //private Point startPoint;
+        //private Rectangle rect;
+
+        //private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    startPoint = e.GetPosition(canvas);
+
+        //    rect = new Rectangle
+        //    {
+        //        Stroke = Brushes.LightBlue,
+        //        StrokeThickness = 2
+        //    };
+        //    Canvas.SetLeft(rect, startPoint.X);
+        //    Canvas.SetTop(rect, startPoint.Y);
+        //    canvas.Children.Add(rect);
+        //}
+
+        //private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    if (e.LeftButton == MouseButtonState.Released || rect == null)
+        //        return;
+
+        //    var pos = e.GetPosition(canvas);
+
+        //    var x = Math.Min(pos.X, startPoint.X);
+        //    var y = Math.Min(pos.Y, startPoint.Y);
+
+        //    var w = Math.Max(pos.X, startPoint.X) - x;
+        //    var h = Math.Max(pos.Y, startPoint.Y) - y;
+
+        //    rect.Width = w;
+        //    rect.Height = h;
+
+        //    Canvas.SetLeft(rect, x);
+        //    Canvas.SetTop(rect, y);
+        //}
+       
+
+        //private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    rect = null;
+        //}
+        #endregion
+
+        private void clkContinuer(object sender, RoutedEventArgs e)
+        {
+            if (choixMenu == 1)
+            {
+                DimensionPiece popup = new DimensionPiece();
+                popup.ShowDialog();
+            }
+            else if(choixMenu == 2)
+            {
+                MessageBox.Show("Option de téléversement à venir!");
+            }
+            else
+            {
+                MessageBox.Show("Veuillez sélectionner un option");
+                //DimensionPiece popup = new DimensionPiece();
+                //popup.ShowDialog();
+            }
+           
+
+
+        }
+
+        private void clkCreerPiece(object sender, RoutedEventArgs e)
+        {
+            choixMenu = 1;
+            btnChoixMenu.BorderBrush = Brushes.DarkBlue;
+            btnChoixMenu2.BorderBrush = Brushes.Transparent;          
+        }
+
+        private void clkTeleverserPiece(object sender, RoutedEventArgs e)
+        {
+            choixMenu = 2;
+            btnChoixMenu2.BorderBrush = Brushes.DarkBlue;
+            btnChoixMenu.BorderBrush = Brushes.Transparent;           
+        }
+    }
+}
